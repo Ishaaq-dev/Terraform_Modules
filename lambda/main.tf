cce56ac7eb853730cefa203bb55b3370d63d0a87
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2025-02-21 17:09:24 +0000

    adds zips file in lambda module

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = var.source_code_path   # Path to your source code folder
-  output_path = "${path.module}/lambda.zip"  # Output path for the ZIP file
+  output_path = "${path.module}/zips/lambda.zip"  # Output path for the ZIP file
 }
 
 resource "aws_lambda_function" "function" {
